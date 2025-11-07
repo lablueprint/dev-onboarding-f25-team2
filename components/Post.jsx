2ea165d230f61d0221d2ac84b06970fcea7c2063
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-06 22:23:20 -0800

    add bookmark icon and press functionality

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,10 +1,25 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
+import { useState } from 'react';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Post() {
+  const [saved, setSaved] = useState(false);
+  
+  const handleOnPress = () => {
+    setSaved(!saved);
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.container}>
-            <Text>This is one post</Text>
+          <View style={styles.headerContainer}>
+              <Text>This is one post</Text>
+              <Pressable onPress={handleOnPress}>
+              { saved ? <FontAwesome name="bookmark" size={24} color="black"/> 
+                    : <Feather name="bookmark" size={24} color="black"/> }
+              </Pressable>
+            </View>
         </View>
     </ScrollView>
   );
@@ -21,5 +36,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: 'black'
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'space-between',
+    justifyContent: 'center'
   }
 });
