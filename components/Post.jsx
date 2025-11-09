3f386fe39ce5940cefb416249e5922334244bd06
kellyy8 <kellyyu2886@gmail.com>
2025-11-09 14:18:21 -0800

    Implement add comment functionality.

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,12 +1,37 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function Post() {
+  const [newComment, setNewComment] = useState('');
+  const [allComments, setAllComments] = useState([]);
+
+  const addComment = () => {
+    if(newComment === ''){
+      return;
+    }
+    setAllComments([...allComments, newComment]);
+    setNewComment('');
+  }
+  
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.container}>
             <Text>This is one post</Text>
-            <AntDesign name="comment" size={20} color="black" />
+            <View style={styles.commentsWrapper}>
+              <TextInput
+                style={styles.comment}
+                placeholder="Enter a comment"
+                value={newComment}
+                onChangeText={setNewComment}
+              />
+              <AntDesign
+                name="comment"
+                size={20}
+                color="black"
+                onPress={addComment}
+              />
+            </View>
         </View>
     </ScrollView>
   );
@@ -23,5 +48,21 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: 'black'
-  }
+  },
+  commentsWrapper: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 10,
+    paddingTop: 20,
+  },
+  comment: {
+    fontSize: 16,
+    padding: 4,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: 200,
+  },
 });
