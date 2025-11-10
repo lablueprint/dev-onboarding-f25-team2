2d904c6b0791b52a407507ec021254bb14ac8271
neelesh <neeleshseerapu@gmail.com>
2025-11-09 21:09:09 -0800

    made it compatible with actual react-native

--- app/login/profile_page.jsx
+++ app/login/profile_page.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const [firstName, setFirstName] = useState("Joe");
 const [lastName, setLastName] = useState("Bruin");
@@ -7,21 +7,25 @@ const [username, setUsername] = useState("jbruin19");
 
 export default function profilePage() {
   return (
-    <View>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text>Profile Page</Text>
       <Image
         source={require("../../assets/images/joebruin.webp")}
         style={styles.ProfilePic}
-      ></Image>
-      <div className="name">
+      />
+      <View style={styles.Name}>
         <Text>{firstName}</Text>
+        <Text> </Text>
         <Text>{lastName}</Text>
-      </div>
+      </View>
       <Text>{username}</Text>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   ProfilePic: { width: 100, height: 100 },
+  Name: { flexDirection: "row" },
+  container: { flex: 1, alignItems: "center", padding: 20 },
+  PageHeader: { fontSize: 28 },
 });
