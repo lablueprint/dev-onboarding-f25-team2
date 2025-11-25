25648704c4f2e2b0f96f73afc217a9f0114e356f
kellyy8 <kellyyu2886@gmail.com>
2025-11-24 18:26:24 -0800

    Move Comment schema into Post schema.

--- backend/models/PostModel.js
+++ backend/models/PostModel.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose')
-const commentSchema = require('./CommentModel')
 
 const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+    {
+        commentText: { // text content of the comment
+            type: String,
+            trim: true,
+            required: true,
+            default: '',
+        },
+        userID: { // user who created the comment
+            type: String,
+        },
+        postID: { // post that the comment belongs to
+            type: Schema.Types.ObjectId,
+            ref: 'Post',
+        },
+    }, 
+    { 
+        timestamps: true,
+    },
+);
 
 const postSchema = new Schema({
     title: {
